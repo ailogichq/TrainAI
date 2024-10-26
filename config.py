ae5c312d0ee6f8738f688a60c06816d0a213c896
SamEmma <emmysamemos256@gmail.com>
2024-10-26 02:09:54 +0100

    secret key setup

--- config.py
+++ config.py
@@ -2,17 +2,10 @@
 Manage all configurations and connections
 """
 
-import os
-from dotenv import load_dotenv
-
-# from pymongo.mongo_client import MongoClient
-
-load_dotenv()
-
+import streamlit as st
 
 class ConnectionConfig:
-    openai_url: str = os.environ.get("OPENAI_URL")
-    openai_sec_key: str = os.environ.get("OPENAI_SEC_KEY")
-
+    openai_url: str = st.secrets["OPENAI_URL"]
+    openai_sec_key: str = st.secrets["OPENAI_SEC_KEY"]
 
 settings = ConnectionConfig()
